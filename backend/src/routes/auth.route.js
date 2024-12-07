ad16628498540417171f3d052a5318b6e6ddd436
saloni0419 <salonisingh0419@gmail.com>
2024-12-07 17:25:39 +0530

    check auth done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  checkAuth,
   login,
   logout,
   signup,
@@ -16,5 +17,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile); // this route is for logged in user
+
+router.get("/check", protectRoute, checkAuth) // route to check if user is authenticated or not
 
 export default router;
