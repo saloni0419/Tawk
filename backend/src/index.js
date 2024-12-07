7e44244ede2f2622d9dd02fc3bc642db371c6bc0
saloni0419 <salonisingh0419@gmail.com>
2024-12-07 16:49:04 +0530

    protect route middleware done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser"
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT;
 
-
+app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,20 @@
-import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js"
+import express from "express";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
 
-router.post("/login", login)
+router.post("/login", login);
 
-router.post("/logout", logout)
+router.post("/logout", logout);
 
-export default router
+router.put("/update-profile", protectRoute, updateProfile); // this route is for logged in user
+
+export default router;
