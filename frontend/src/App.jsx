26c15399f2f6f60382efe8c6819d851d64e1c779
saloni0419 <salonisingh0419@gmail.com>
2024-12-15 14:35:11 +0530

    fe routes and pages setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 const App = () => {
   return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </div>
+   <div>
+
+    <Navbar />
+
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/signup" element={<SignUpPage />}/>
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/profile" element={<ProfilePage />}/>
+      <Route path="/settings" element={<SettingsPage />}/>
+    </Routes>
+
+   </div>
   );
 };
 
