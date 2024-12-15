8b2c1ffdbddce0cc95004593494860bae56dd2be
saloni0419 <salonisingh0419@gmail.com>
2024-12-15 13:55:51 +0530

    send message controller done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -18,22 +18,55 @@ export const getUsersForSidebar = async (req, res) => {
 };
 
 // get all messages between two users
-export const getMessages = async (req,res) => {
+export const getMessages = async (req, res) => {
   try {
-    const {id : userToChatId} = req.params // renamed to userToChatId
-    const myId = req.user._id // id of logged in user
+    const { id: userToChatId } = req.params; // renamed to userToChatId
+    const myId = req.user._id; // id of logged in user
 
     // find all the messages where i am the sender or the other user is sender
     const messages = await Message.find({
-      $or : [
-        {senderId : myId , receiverId : userToChatId},
-        {senderId : userToChatId , receiverId : myId}
-      ]
-    })
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
 
-    res.status(200).json(messages)
+    res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// message could be both text and image
+export const sendMessage = async (req, res) => {
+  try {
+    const {text, image} = req.params
+    const {id : receiverId} = req.params // receiverId is id where i am sending the message
+    const senderId = req.user._id
+
+    // check if user is sending image or not
+    let imageUrl
+    if(image){
+      const uploadResponse = await cloudinary.uploader.upload(image) // uploading image to cloudinary
+      imageUrl = uploadResponse.secure_url
+    }
+
+    // create a new message
+    const newMessage =  new Message({
+      senderId,
+      receiverId,
+      text,
+      image : imageUrl
+    })
+
+    await newMessage.save()
+
+    // todo -> realtime functionality using socket.io
+
+    res.status(201).json(newMessage)
+  } catch (error) {
+    console.log("Error in sendMessage controller ", error.message);
     res.status(500).json({error : "Internal server error"})
   }
-}
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,17 @@
-import express from "express"
-import { protectRoute } from "../middleware/protectRoute.middleware.js"
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js"
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.middleware.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/users", protectRoute, getUsersForSidebar) // get every user on contact list
+router.get("/users", protectRoute, getUsersForSidebar); // get every user on contact list
 
-router.get("/:id", protectRoute, getMessages) // message between two user
+router.get("/:id", protectRoute, getMessages); // message between two user
 
+router.post("/send/:id", protectRoute, sendMessage) // send message to other user
 
-export default router
+export default router;
